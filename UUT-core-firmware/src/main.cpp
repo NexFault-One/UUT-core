ca682c1ae8a20c4765da3d7fbd8a556251112fdd
Nizar Zariouh <nizarzariouh05@gmail.com>
2026-02-23 04:32:12 -0500

    changed for modbus (to be tested)

--- UUT-core-firmware/src/main.cpp
+++ UUT-core-firmware/src/main.cpp
@@ -1,8 +1,7 @@
 #include <Arduino.h>
-#include "../include/proto_codec/proto_communication.h"
-#include "../include/proto_codec/proto_communication.c"
-#include "../protobuf_msgs/proto_msgs/uart_data.pb.c"
-
+#include <ModbusRTU.h>
+#include "proto_codec/proto_communication.h"
+#include "proto_msgs/uart_data.pb.h"
 
 // nizar esp pins
 #define TX_PIN_NZ 17
@@ -12,12 +11,31 @@
 #define TX_PIN 6
 #define RX_PIN 5
 
+// modbus pins
+#define MODBUS_RX 15
+#define MODBUS_TX 7
+#define MODBUS_DE 42
+
 // bauds
 #define HOST_BAUD 9600
+#define MODBUS_BAUD 19200
 #define DEVICE_BAUD 115200
 
 // to identify the esp32 for com port
 #define DEVICE_ID "UUT"
+#define SLAVE_ID 1
+
+// registers for modbus for debug
+#define REG_GENERIC 100
+
+ModbusRTU mb;
+
+// writing
+uint16_t cbGeneric(TRegister* reg, uint16_t val)
+{
+  Serial.printf("[UUT] [CLEAN WRITE] Register %d (Generic) updated to: %u\n", REG_GENERIC, val);
+  return val;
+}
 
 void performHandshake()
 {
@@ -45,11 +63,25 @@ void performHandshake()
 void setup() {
   // host connection debug console
   Serial.begin(HOST_BAUD);
+  // MODBUS
+  Serial1.begin(MODBUS_BAUD, SERIAL_8N1, MODBUS_RX, MODBUS_TX);
   // UART used to talk with DSI
   Serial2.begin(DEVICE_BAUD, SERIAL_8N1, RX_PIN, TX_PIN);
   delay(500);
 
-  performHandshake();
+  pinMode(MODBUS_DE, OUTPUT);
+  digitalWrite(MODBUS_DE, LOW);
+
+  mb.begin(&Serial1, MODBUS_DE);
+
+  mb.slave(SLAVE_ID);
+
+  mb.addHreg(REG_GENERIC, 0);
+  mb.onSetHreg(REG_GENERIC, cbGeneric);
+
+  Serial.printf("[UUT] Listening on Serial1 (Slave ID: %d)...\n", SLAVE_ID);
+
+  //performHandshake();
   
   Serial.println("[UUT] HANDSHAKE COMPLETED. UUT: Ready.");
 }
@@ -81,35 +113,34 @@ void setup() {
 // use for now -- temporary
 void loop()
 {
-    if (Serial2.available() > 0)
-    {
-        uint8_t buffer[PROTOBUF_BUFFER_SIZE];
-        size_t bytes_read = 0;
-        unsigned long last_byte_time = millis();
+  mb.task();
 
-        // keep reading until no new bytes arrive for 50 ms or buffer full
-        while ((millis() - last_byte_time) < 50 && bytes_read < PROTOBUF_BUFFER_SIZE)
-        {
-            if (Serial2.available())
-            {
-                buffer[bytes_read++] = Serial2.read();
-                last_byte_time = millis();  // reset timer after each byte
-            }
-        }
-
-        if (bytes_read == 0)
-            return;
-
-        Serial.printf("RX bytes (%u):\n", bytes_read);
-        for (size_t i = 0; i < bytes_read; ++i)
-            Serial.printf("%02X ", buffer[i]);
-        Serial.println();
-
-        Serial.print("ASCII message: ");
-        for (size_t i = 0; i < bytes_read; ++i)
-            Serial.write(buffer[i]);
-        Serial.println();
-    }
-
-    delay(10);
+  yield();
+  
+  if (Serial1.available() > 0)
+  {
+      uint8_t buffer[PROTOBUF_BUFFER_SIZE];
+      size_t bytes_read = 0;
+      unsigned long last_byte_time = millis();
+      // keep reading until no new bytes arrive for 50 ms or buffer full
+      while ((millis() - last_byte_time) < 50 && bytes_read < PROTOBUF_BUFFER_SIZE)
+      {
+          if (Serial1.available())
+          {
+              buffer[bytes_read++] = Serial1.read();
+              last_byte_time = millis();  // reset timer after each byte
+          }
+      }
+      if (bytes_read == 0)
+          return;
+      Serial.printf("RX bytes (%u):\n", bytes_read);
+      for (size_t i = 0; i < bytes_read; ++i)
+          Serial.printf("%02X ", buffer[i]);
+      Serial.println();
+      Serial.print("ASCII message: ");
+      for (size_t i = 0; i < bytes_read; ++i)
+          Serial.write(buffer[i]);
+      Serial.println();
+  }
+  delay(10);
 }
